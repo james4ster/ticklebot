15fa1188bb030da62e1faadb19b9a9c233c68967
Jamie Forrester <35127046-james4ster@users.noreply.replit.com>
2025-08-28 15:26:00 +0000

    updated fetchelo to use correct variable key with json on the end

--- nhl95-elo-chart/fetchELO.js
+++ nhl95-elo-chart/fetchELO.js
@@ -1,7 +1,7 @@
 import { google } from 'googleapis';
 
 // Instead of reading a local JSON file, parse it from an environment variable
-const raw = process.env.GOOGLE_SERVICE_ACCOUNT;
+const raw = process.env.GOOGLE_SERVICE_ACCOUNT_JSON;
 const serviceAccount = JSON.parse(raw.replace(/\\n/g, '\n'));
 
 const SCOPES = ['https://www.googleapis.com/auth/spreadsheets.readonly'];
